0c55e374ed19f32cab75c240372769624eebb2c2
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-09-11 10:52:35 +0300

    commit new

---
+++ SpaceShop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceShop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] public IActionResult Create()
+        {
+            return View();
+        
+        }
+
+    }
+}
