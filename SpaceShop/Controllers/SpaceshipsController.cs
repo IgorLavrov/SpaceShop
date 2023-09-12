06c75423cd17db029e3f7552fa81932096c846bf
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-09-13 00:28:15 +0300

    commit

--- SpaceShop/Controllers/SpaceshipsController.cs
+++ SpaceShop/Controllers/SpaceshipsController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
 
 namespace SpaceShop.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopContext _context;
+        private readonly ISpaceshipServices _spaceshipServices;
         public IActionResult Index()
         {
             return View();
