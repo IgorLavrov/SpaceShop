9134d3ef5c50c6c7fd3dca231b653c758e9eb2a4
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-09-18 08:54:39 +0300

    Commit Services updated

--- SpaceShop/Program.cs
+++ SpaceShop/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Shop.ApplicationServices.Services;
+using Shop.Core.ServiceInterface;
 using Shop.Data;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,13 +12,17 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ShopContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
